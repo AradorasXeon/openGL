91e23308a2c11e065f8256a5b1226cdd82ae7994
Krisztián József Varga <aradorasxeon@gmail.com>
2023-09-25 23:16:20 +0200

    first part of video

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "../include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
+//Based on: https://www.youtube.com/watch?v=45MIykWJ-C4
+
 int main()
 {
     //std::cout << "Hello there :)" << std::endl;
@@ -13,17 +15,28 @@ int main()
         std::cerr << "glfw failed to init." << std::endl;
         return -1;
     }
+    glfwWindowHint(GLFW_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_VERSION_MINOR, 8);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Could not create window with this
 
-    window = glfwCreateWindow(640, 480, "Beautiful window", NULL, NULL);
+    window = glfwCreateWindow(800, 800, "Beautiful window", NULL, NULL);
+    if(window == nullptr)
+    {
+        std::cerr << "Could not create window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
     glfwMakeContextCurrent(window);
 
-    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) //this loads openGL
     {
         std::cerr << "Glad loader failed to get proc address" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    glViewport(0,0,450,600); //hm so far this does nothing, or so it seems
     glClearColor(0.25f, 0.82f, 0.83f, 1.0f);
 
     while(!glfwWindowShouldClose(window))
@@ -33,6 +46,8 @@ int main()
         glfwSwapBuffers(window);
     }
 
+
+    glfwDestroyWindow(window);
     glfwTerminate();
 
 
