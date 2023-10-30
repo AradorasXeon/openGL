6208682f227e35109bd4879845396347e321788e
Krisztián József Varga <aradorasxeon@gmail.com>
2023-10-30 21:21:47 +0100

    Camera

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "../include/src/VBO.h"
 #include "../include/src/EBO.h"
 #include "../include/src/Texture.h"
+#include "../include/src/Camera.h"
 
 //Based on: https://www.youtube.com/watch?v=45MIykWJ-C4
 
@@ -85,45 +86,23 @@ int main()
     VBO1.Unbind();
     EBO1.Unbind();
 
-    GLuint uniID = glGetUniformLocation(shaderProgram.getID() , "scale"); //to get the location of "scale" from the default.vert
-
     //Texture
     Texture superImage("../resources/images/earth.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
     superImage.TexUnit(shaderProgram, "tex0", 0);
 
-    float rotation = 0.0f;
-    double prevTime = glfwGetTime();
-
     glEnable(GL_DEPTH_TEST); //enable depth buffer, so we don't get random X-ray vision
     
+    Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
     while(!glfwWindowShouldClose(window))
     {
         glClearColor(0.65f, 0.65f, 0.65f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         shaderProgram.Activate();
 
-        double currentTime = glfwGetTime();
-        if(currentTime - prevTime >= 1/60)
-        {
-            rotation += 0.5f;
-            prevTime = currentTime;
-        }
+        camera.Inputs(window);
+        camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
 
-        glm::mat4 model = glm::mat4(1.0f);
-        glm::mat4 view = glm::mat4(1.0f);
-        glm::mat4 proj = glm::mat4(1.0f);
-        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
-        proj = glm::perspective(glm::radians(45.0f), (float)(width/height), 0.1f, 100.0f); //field of view, aspect ratio, near clip, far clip
-
-        int modelLoc = glGetUniformLocation(shaderProgram.getID(), "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        int viewLoc = glGetUniformLocation(shaderProgram.getID(), "view");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        int projLoc = glGetUniformLocation(shaderProgram.getID(), "proj");
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
-
-        glUniform1f(uniID, 0.5f); //can be used only after shader program is activated
         superImage.Bind();
         VAO1.Bind();
         glDrawElements(GL_TRIANGLES, sizeof(indicies)/sizeof(int), GL_UNSIGNED_INT, 0); //not glDrawArrays, so EBO is used
