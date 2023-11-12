3a66d8a60dcf431c1cafb38a0d7eab86cb3d5e58
Krisztián József Varga <aradorasxeon@gmail.com>
2023-11-12 20:32:11 +0100

    lighting

--- shaders/light.frag
+++ shaders/light.frag
@@ -2,8 +2,10 @@
 
 out vec4 FragColor;
 
+uniform vec4 lightColor;
+
 void main()
 {
-    FragColor = vec4(1.0f,1.0f,1.0f,1.0f);
+    FragColor = lightColor;
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -33,23 +33,38 @@ int main()
     glfwWindowHint(GLFW_VERSION_MINOR, 8);
     //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Could not create window with this
 
-    GLfloat verticies[]=
-    {
-        -0.5f, 0.0f, 0.5f,          0.83f, 0.70f, 0.44f,   0.0f, 0.0f,
-        -0.5f, 0.0f, -0.5f,         0.95f, 0.70f, 0.44f,   7.0f, 0.0f,
-        0.5f, 0.0f, -0.5f,          0.95f, 0.89f, 0.44f,   0.0f, 0.0f,
-        0.5f, 0.0f, 0.5f,           0.95f, 0.89f, 0.2f,    3.0f, 0.0f,
-        0.0f, 0.8f, 0.0f,           0.25f, 0.25f, 0.89f,   2.5f, 5.0f
+    GLfloat verticies[] =
+    { //     COORDINATES     /        COLORS          /    TexCoord   /        NORMALS       //
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 5.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+        0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, -1.0f, 0.0f, // Bottom side
+
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,     -0.8f, 0.5f,  0.0f, // Left Side
+
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.0f, 0.5f, -0.8f, // Non-facing side
+
+        0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	 0.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+        0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.8f, 0.5f,  0.0f, // Right side
+        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.8f, 0.5f,  0.0f, // Right side
+
+        0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	 5.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 	 0.0f, 0.0f,      0.0f, 0.5f,  0.8f, // Facing side
+        0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	 2.5f, 5.0f,      0.0f, 0.5f,  0.8f  // Facing side
     };
 
     GLuint indicies[] =
     {
-        0,1,2,
-        0,2,3,
-        0,1,4,
-        1,2,4,
-        2,3,4,
-        3,0,4
+        0, 1, 2, // Bottom side
+        0, 2, 3, // Bottom side
+        4, 6, 5, // Left side
+        7, 9, 8, // Non-facing side
+        10, 12, 11, // Right side
+        13, 15, 14 // Facing side
     };
 
     GLfloat lightVerticies[] = 
@@ -107,9 +122,10 @@ int main()
     VBO VBO1(verticies, sizeof(verticies));
     EBO EBO1(indicies, sizeof(indicies));   //will be automaticaly (?) attached to VAO
 
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(GLfloat), (void*)0); //first 3 numbers from verticies
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat))); //next 3 nums
-    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat))); //next 2 nums
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 11 * sizeof(GLfloat), (void*)0); //first 3 numbers from verticies
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 11 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat))); //next 3 nums
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 11 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat))); //next 2 nums
+    VAO1.LinkAttrib(VBO1, 3, 3, GL_FLOAT, 11 * sizeof(GLfloat), (void*)(8 * sizeof(GLfloat))); //next 2 nums
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
@@ -127,6 +143,8 @@ int main()
     lightVBO.Unbind();
     lightEBO.Unbind();
 
+    glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+
     glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
     glm::mat4 lightModel = glm::mat4(1.0f);
     lightModel = glm::translate(lightModel, lightPos);
@@ -137,9 +155,12 @@ int main()
 
     lightShader.Activate();
     glUniformMatrix4fv(glGetUniformLocation(lightShader.getID(), "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+    glUniform4f(glGetUniformLocation(lightShader.getID(), "lightColor"),lightColor.x,lightColor.y,lightColor.z,lightColor.w);
 
     shaderProgram.Activate();
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram.getID(), "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+    glUniform4f(glGetUniformLocation(shaderProgram.getID(), "lightColor"),lightColor.x,lightColor.y,lightColor.z,lightColor.w);
+    glUniform3f(glGetUniformLocation(shaderProgram.getID(), "lightPos"),lightPos.x,lightPos.y,lightPos.z);
 
 
     //Texture
@@ -158,7 +179,7 @@ int main()
         camera.Inputs(window);
         camera.updateMatrix(45.0f, 0.1f, 100.0f);
         shaderProgram.Activate();
-
+        glUniform3f(glGetUniformLocation(shaderProgram.getID(), "camPos"), camera.Position.x,camera.Position.y,camera.Position.z);
         camera.Matrix(shaderProgram, "camMatrix");
 
         superImage.Bind();
